debe053f17fdba7acf9d94963b75e5e337907a79
staszkiet <zaprzalskistanislaw@gmail.com>
2025-02-24 13:13:01 +0100

    Add chain of responsibility skeleton

--- inputHandler.go
+++ inputHandler.go
@@ -12,8 +12,22 @@ type AddHandler struct {
 	next IHandler
 }
 
+type DeleteHandler struct {
+	next IHandler
+}
+
+type CreateHandler struct {
+	next IHandler
+}
+
+type UpdateHandler struct {
+	next IHandler
+}
+
 func (a AddHandler) PerformAction(word string, action string) bool {
 	if action == "ADD" {
+
+		//perform action
 		fmt.Println(word)
 		return true
 	}
@@ -24,10 +38,52 @@ func (a AddHandler) PerformAction(word string, action string) bool {
 	}
 }
 
+func (c CreateHandler) PerformAction(word string, action string) bool {
+	if action == "CREATE" {
+
+		//perform action
+		fmt.Println(word)
+		return true
+	}
+	if c.next == nil {
+		return false
+	} else {
+		return c.next.PerformAction(word, action)
+	}
+}
+
+func (d DeleteHandler) PerformAction(word string, action string) bool {
+	if action == "DELETE" {
+
+		//perform action
+		fmt.Println(word)
+		return true
+	}
+	if d.next == nil {
+		return false
+	} else {
+		return d.next.PerformAction(word, action)
+	}
+}
+
+func (u UpdateHandler) PerformAction(word string, action string) bool {
+	if action == "UPDATE" {
+
+		//perform action
+		fmt.Println(word)
+		return true
+	}
+	if u.next == nil {
+		return false
+	} else {
+		return u.next.PerformAction(word, action)
+	}
+}
+
 func ListenForInput() {
 	var action string
 	var word string
-	add := AddHandler{next: nil}
+	add := AddHandler{next: CreateHandler{next: DeleteHandler{next: UpdateHandler{}}}}
 	for {
 
 		fmt.Println("choose action:")
