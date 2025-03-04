a7ccf5a13777c69663a3512372acfd61a8833e10
staszkiet <zaprzalskistanislaw@gmail.com>
2025-03-04 22:03:53 +0100

    Add meaningful error messages for GORM errors

--- server/database/database.go
+++ server/database/database.go
@@ -1,11 +1,14 @@
 package database
 
 import (
+	"errors"
+	"fmt"
 	"log"
 	"os"
 
 	"github.com/joho/godotenv"
 	dbmodels "github.com/staszkiet/DictionaryGolang/server/database/models"
+	customerrors "github.com/staszkiet/DictionaryGolang/server/errors"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -32,4 +35,40 @@ func ConnectDB() *gorm.DB {
 
 	return db
 
+}
+
+type ErrorOptions struct {
+	Polish   string
+	English  string
+	Sentence string
+}
+
+func CheckWhichDoesntExits(eo ErrorOptions, tx *gorm.DB) error {
+	var word dbmodels.Word
+	var translation dbmodels.Translation
+	var sentence dbmodels.Sentence
+	err := tx.Model(&dbmodels.Word{}).Where("polish = ?", eo.Polish).First(&word).Error
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return customerrors.WordNotExistsError{Word: eo.Polish}
+		}
+		return err
+	}
+	err = tx.Model(&dbmodels.Translation{}).Where("english = ? AND word_id = ?", eo.English, word.ID).First(&translation).Error
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return customerrors.TranslationNotExistsError{Word: eo.Polish, Translation: eo.English}
+		}
+		return err
+	}
+	if eo.Sentence != "" {
+		err = tx.Model(&dbmodels.Sentence{}).Where("sentence = ? AND translation_id = ?", eo.Sentence, translation.ID).First(&sentence).Error
+		if err != nil {
+			if errors.Is(err, gorm.ErrRecordNotFound) {
+				return customerrors.SentenceNotExistsError{Word: eo.Polish, Translation: eo.English, Sentence: eo.Sentence}
+			}
+			return err
+		}
+	}
+	return fmt.Errorf("nieznany błąd")
 }
