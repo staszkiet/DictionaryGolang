08b14e4b274f1fcd625dec1937d208c7bfa27a49
staszkiet <zaprzalskistanislaw@gmail.com>
2025-03-05 19:47:25 +0100

    Add transaction initialization to seperate function

--- server/database/database.go
+++ server/database/database.go
@@ -2,34 +2,18 @@ package database
 
 import (
 	"context"
-	"errors"
-	"fmt"
 	"log"
 	"os"
 
 	"github.com/joho/godotenv"
 	dbmodels "github.com/staszkiet/DictionaryGolang/server/database/models"
-	customerrors "github.com/staszkiet/DictionaryGolang/server/errors"
 	"github.com/staszkiet/DictionaryGolang/server/graph/model"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
-type IDictionary interface {
-	CreateWord(ctx context.Context, polish string, translation model.NewTranslation) (bool, error)
-	CreateSentence(ctx context.Context, polish string, english string, sentence string) (bool, error)
-	CreateTranslation(ctx context.Context, polish string, translation model.NewTranslation) (bool, error)
-	DeleteSentence(ctx context.Context, polish string, english string, sentence string) (bool, error)
-	DeleteTranslation(ctx context.Context, polish string, english string) (bool, error)
-	DeleteWord(ctx context.Context, polish string) (bool, error)
-	UpdateTranslation(ctx context.Context, polish string, english string, newEnglish string) (bool, error)
-	UpdateWord(ctx context.Context, polish string, newPolish string) (bool, error)
-	SelectWord(ctx context.Context, polish string) (*model.Word, error)
-}
-
 type DatabaseService struct {
 	queryHandler *dictionaryRepository
-	DB           *gorm.DB
 }
 
 func NewDatabaseService() *DatabaseService {
@@ -51,389 +35,215 @@ func NewDatabaseService() *DatabaseService {
 	if err != nil {
 		log.Fatal("Failed to migrate")
 	}
-
-	return &DatabaseService{DB: db}
+	qh := &dictionaryRepository{DB: db}
+	return &DatabaseService{queryHandler: qh}
 
 }
 
 func (r *DatabaseService) CreateWord(ctx context.Context, polish string, translation model.NewTranslation) (bool, error) {
 
-	tx := r.DB.Begin()
-	defer func() {
-		if r := recover(); r != nil {
-			tx.Rollback()
+	return r.queryHandler.WithTransaction(func(tx *gorm.DB) error {
+		sentences := make([]dbmodels.Sentence, 0)
+
+		for _, s := range translation.Sentences {
+			sentences = append(sentences, dbmodels.Sentence{Sentence: s})
 		}
-	}()
 
-	if err := tx.Error; err != nil {
-		return false, err
-	}
+		var convertedTranslations []dbmodels.Translation
 
-	sentences := make([]dbmodels.Sentence, 0)
+		convertedTranslations = append(convertedTranslations, dbmodels.Translation{
+			English:   translation.English,
+			Sentences: sentences,
+		})
 
-	for _, s := range translation.Sentences {
-		sentences = append(sentences, dbmodels.Sentence{Sentence: s})
-	}
+		word := &dbmodels.Word{
+			Polish:       polish,
+			Translations: convertedTranslations,
+		}
 
-	var convertedTranslations []dbmodels.Translation
-
-	convertedTranslations = append(convertedTranslations, dbmodels.Translation{
-		English:   translation.English,
-		Sentences: sentences,
+		if err := r.queryHandler.CreateWord(tx, word); err != nil {
+			tx.Rollback()
+			return err
+		}
+		return nil
 	})
-
-	word := &dbmodels.Word{
-		Polish:       polish,
-		Translations: convertedTranslations,
-	}
-
-	if err := r.queryHandler.CreateWord(tx, word); err != nil {
-		tx.Rollback()
-		return false, err
-	}
-
-	return true, tx.Commit().Error
 }
 
 func (r *DatabaseService) CreateSentence(ctx context.Context, polish string, english string, sentence string) (bool, error) {
 	var word dbmodels.Word
 
-	tx := r.DB.Begin()
-	defer func() {
-		if r := recover(); r != nil {
+	return r.queryHandler.WithTransaction(func(tx *gorm.DB) error {
+		err := r.queryHandler.GetWord(tx, polish, &word)
+		if err != nil {
 			tx.Rollback()
+			return err
 		}
-	}()
 
-	if err := tx.Error; err != nil {
-		return false, err
-	}
-
-	err := r.queryHandler.GetWord(tx, polish, &word)
-	if err != nil {
-		tx.Rollback()
-		return false, err
-	}
-
-	for i, t := range word.Translations {
-		if t.English == english {
-			word.Translations[i].Sentences = append(word.Translations[i].Sentences, dbmodels.Sentence{Sentence: sentence})
+		for i, t := range word.Translations {
+			if t.English == english {
+				word.Translations[i].Sentences = append(word.Translations[i].Sentences, dbmodels.Sentence{Sentence: sentence})
+			}
 		}
-	}
 
-	if err := r.queryHandler.AddSentence(tx, &word, english, sentence); err != nil {
-		tx.Rollback()
-		return false, err
-	}
+		if err := r.queryHandler.AddSentence(tx, &word, english, sentence); err != nil {
+			tx.Rollback()
+			return err
+		}
+		return nil
+	})
 
-	return true, tx.Commit().Error
 }
 
 func (r *DatabaseService) CreateTranslation(ctx context.Context, polish string, translation model.NewTranslation) (bool, error) {
-	var word dbmodels.Word
-	sentences := make([]dbmodels.Sentence, 0)
 
-	for _, s := range translation.Sentences {
-		sentences = append(sentences, dbmodels.Sentence{Sentence: s})
-	}
+	return r.queryHandler.WithTransaction(func(tx *gorm.DB) error {
+		var word dbmodels.Word
+		sentences := make([]dbmodels.Sentence, 0)
 
-	tx := r.DB.Begin()
-	defer func() {
-		if r := recover(); r != nil {
-			tx.Rollback()
+		for _, s := range translation.Sentences {
+			sentences = append(sentences, dbmodels.Sentence{Sentence: s})
 		}
-	}()
+		err := r.queryHandler.GetWord(tx, polish, &word)
+		if err != nil {
+			tx.Rollback()
+			return err
+		}
 
-	if err := tx.Error; err != nil {
-		return false, err
-	}
+		word.Translations = append(word.Translations, dbmodels.Translation{
+			English:   translation.English,
+			Sentences: sentences,
+		})
 
-	err := r.queryHandler.GetWord(tx, polish, &word)
-	if err != nil {
-		tx.Rollback()
-		return false, err
-	}
-
-	word.Translations = append(word.Translations, dbmodels.Translation{
-		English:   translation.English,
-		Sentences: sentences,
+		if err = r.queryHandler.AddTranslation(tx, &word, translation.English); err != nil {
+			tx.Rollback()
+			return err
+		}
+		return nil
 	})
 
-	if err = r.queryHandler.AddTranslation(tx, &word, translation.English); err != nil {
-		tx.Rollback()
-		return false, err
-	}
-
-	return true, tx.Commit().Error
 }
 
 func (r *DatabaseService) DeleteSentence(ctx context.Context, polish string, english string, sentence string) (bool, error) {
 
-	var s dbmodels.Sentence
-
-	tx := r.DB.Begin()
-	defer func() {
-		if r := recover(); r != nil {
+	return r.queryHandler.WithTransaction(func(tx *gorm.DB) error {
+		var s dbmodels.Sentence
+		err := r.queryHandler.GetSentence(tx, polish, english, sentence, &s)
+		if err != nil {
 			tx.Rollback()
+			return err
 		}
-	}()
 
-	if err := tx.Error; err != nil {
-		return false, err
-	}
-
-	err := tx.Joins("JOIN translations ON sentences.translation_id = translations.id").
-		Joins("JOIN words ON words.id = translations.word_id").
-		Where("words.polish = ? AND translations.english = ? AND sentences.sentence = ?", polish, english, sentence).
-		First(&s).Error
-	if err != nil {
-		tx.Rollback()
-		if errors.Is(err, gorm.ErrRecordNotFound) {
-			return false, r.CheckWhichDoesntExits(ErrorOptions{Polish: polish, English: english, Sentence: sentence}, r.DB)
+		if err := r.queryHandler.DeleteSentence(tx, s); err != nil {
+			tx.Rollback()
+			return err
 		}
-		return false, err
-	}
+		return nil
+	})
 
-	if err := tx.Delete(s).Error; err != nil {
-		tx.Rollback()
-		return false, err
-	}
-
-	return true, tx.Commit().Error
 }
 
 func (r *DatabaseService) DeleteTranslation(ctx context.Context, polish string, english string) (bool, error) {
-	var translation dbmodels.Translation
-	var count int64
 
-	tx := r.DB.Begin()
-	defer func() {
-		if r := recover(); r != nil {
+	return r.queryHandler.WithTransaction(func(tx *gorm.DB) error {
+		var translation dbmodels.Translation
+		err := r.queryHandler.GetTranslation(tx, polish, english, &translation)
+		if err != nil {
 			tx.Rollback()
+			return err
 		}
-	}()
 
-	if err := tx.Error; err != nil {
-		return false, err
-	}
-
-	err := tx.Joins("RIGHT JOIN words ON words.id = translations.word_id").
-		Where("words.polish = ? AND translations.english = ?", polish, english).
-		First(&translation).Error
-	if err != nil {
-		tx.Rollback()
-		if errors.Is(err, gorm.ErrRecordNotFound) {
-			return false, r.CheckWhichDoesntExits(ErrorOptions{Polish: polish, English: english}, r.DB)
-		}
-		return false, err
-	}
-
-	if err := tx.Model(&translation).Delete(&translation).Error; err != nil {
-		tx.Rollback()
-		return false, err
-	}
-
-	if err := tx.Model(&dbmodels.Translation{}).Where("word_id = ?", translation.WordID).Count(&count).Error; err != nil {
-		tx.Rollback()
-		return false, err
-	}
-
-	if count == 0 {
-		if err := tx.Where("ID = ?", translation.WordID).Delete(&dbmodels.Word{}).Error; err != nil {
+		if err := r.queryHandler.DeleteTranslation(tx, &translation); err != nil {
 			tx.Rollback()
-			return false, err
+			return err
 		}
-	}
+		return nil
+	})
 
-	return true, tx.Commit().Error
 }
 
 func (r *DatabaseService) DeleteWord(ctx context.Context, polish string) (bool, error) {
-	var word dbmodels.Word
 
-	tx := r.DB.Begin()
-	defer func() {
-		if r := recover(); r != nil {
+	return r.queryHandler.WithTransaction(func(tx *gorm.DB) error {
+		if err := r.queryHandler.DeleteWord(tx, polish); err != nil {
 			tx.Rollback()
+			return err
 		}
-	}()
-
-	if err := tx.Error; err != nil {
-		return false, err
-	}
-
-	if err := tx.Where("polish = ?", polish).First(&word).Error; err != nil {
-		if errors.Is(err, gorm.ErrRecordNotFound) {
-			return false, customerrors.WordNotExistsError{Word: polish}
-		}
-		return false, err
-	}
-
-	if err := tx.Delete(&word).Error; err != nil {
-		tx.Rollback()
-		return false, err
-	}
-
-	return true, tx.Commit().Error
+		return nil
+	})
 }
 
 func (r *DatabaseService) UpdateWord(ctx context.Context, polish string, newPolish string) (bool, error) {
-	var word dbmodels.Word
 
-	tx := r.DB.Begin()
-	defer func() {
-		if r := recover(); r != nil {
+	return r.queryHandler.WithTransaction(func(tx *gorm.DB) error {
+		var word dbmodels.Word
+		err := r.queryHandler.GetWord(tx, polish, &word)
+		if err != nil {
 			tx.Rollback()
+			return err
 		}
-	}()
 
-	if err := tx.Error; err != nil {
-		return false, err
-	}
-
-	err := tx.Model(&dbmodels.Word{}).Where("polish = ?", polish).First(&word).Error
-	if err != nil {
-		tx.Rollback()
-		if errors.Is(err, gorm.ErrRecordNotFound) {
-			return false, customerrors.WordNotExistsError{Word: polish}
+		if err := r.queryHandler.UpdateWord(tx, &word, newPolish); err != nil {
+			tx.Rollback()
+			return err
 		}
-		return false, err
-	}
+		return nil
+	})
 
-	if err := tx.Model(&word).Update("polish", newPolish).Error; err != nil {
-		tx.Rollback()
-		return false, err
-	}
-
-	return true, tx.Commit().Error
 }
 
 func (r *DatabaseService) UpdateTranslation(ctx context.Context, polish string, english string, newEnglish string) (bool, error) {
-	var translation dbmodels.Translation
 
-	tx := r.DB.Begin()
-	defer func() {
-		if r := recover(); r != nil {
+	return r.queryHandler.WithTransaction(func(tx *gorm.DB) error {
+		var translation dbmodels.Translation
+
+		err := r.queryHandler.GetTranslation(tx, polish, english, &translation)
+
+		if err != nil {
 			tx.Rollback()
+			return err
 		}
-	}()
 
-	if err := tx.Error; err != nil {
-		return false, err
-	}
-
-	err := tx.Joins("RIGHT JOIN words ON words.id = translations.word_id").
-		Where("words.polish = ? AND translations.english = ?", polish, english).
-		First(&translation).Error
-
-	if err != nil {
-		tx.Rollback()
-		if errors.Is(err, gorm.ErrRecordNotFound) {
-			return false, r.CheckWhichDoesntExits(ErrorOptions{Polish: polish, English: english}, r.DB)
+		err = r.queryHandler.UpdateTranslation(tx, &translation, newEnglish)
+		if err != nil {
+			tx.Rollback()
+			return err
 		}
-		return false, err
-	}
+		return nil
+	})
 
-	err = tx.Model(&translation).Update("english", newEnglish).Error
-	if err != nil {
-		tx.Rollback()
-		return false, err
-	}
-
-	return true, tx.Commit().Error
 }
 
 func (r *DatabaseService) UpdateSentence(ctx context.Context, polish string, english string, sentence string, newSentence string) (bool, error) {
-	var s dbmodels.Sentence
 
-	tx := r.DB.Begin()
-	defer func() {
-		if r := recover(); r != nil {
+	return r.queryHandler.WithTransaction(func(tx *gorm.DB) error {
+
+		var s dbmodels.Sentence
+		err := r.queryHandler.GetSentence(tx, polish, english, sentence, &s)
+
+		if err != nil {
 			tx.Rollback()
+			return err
 		}
-	}()
 
-	if err := tx.Error; err != nil {
-		return false, err
-	}
-
-	err := tx.Joins("JOIN translations ON sentences.translation_id = translations.id").
-		Joins("JOIN words ON words.id = translations.word_id").
-		Where("words.polish = ? AND translations.english = ? AND sentences.sentence = ?", polish, english, sentence).
-		First(&s).Error
-
-	if err != nil {
-		if errors.Is(err, gorm.ErrRecordNotFound) {
-			return false, r.CheckWhichDoesntExits(ErrorOptions{Polish: polish, English: english, Sentence: sentence}, r.DB)
+		err = r.queryHandler.UpdateSentence(tx, &s, newSentence)
+		if err != nil {
+			tx.Rollback()
+			return err
 		}
-		return false, err
-	}
+		return nil
+	})
 
-	err = tx.Model(&s).Update("sentence", newSentence).Error
-	if err != nil {
-		tx.Rollback()
-		return false, err
-	}
-
-	return true, tx.Commit().Error
 }
 
 func (r *DatabaseService) SelectWord(ctx context.Context, polish string) (*model.Word, error) {
 	var word dbmodels.Word
 
-	tx := r.DB.Begin()
-	defer func() {
-		if r := recover(); r != nil {
+	r.queryHandler.WithTransaction(func(tx *gorm.DB) error {
+		if err := r.queryHandler.GetWord(tx, polish, &word); err != nil {
 			tx.Rollback()
-		}
-	}()
-
-	if err := tx.Error; err != nil {
-		return nil, err
-	}
-
-	if err := tx.Preload("Translations.Sentences").Where("polish = ?", polish).First(&word).Error; err != nil {
-		if errors.Is(err, gorm.ErrRecordNotFound) {
-			return nil, customerrors.WordNotExistsError{Word: polish}
-		}
-		return nil, err
-	}
-
-	return dbmodels.DBWordToGQLWord(&word), tx.Commit().Error
-}
-
-type ErrorOptions struct {
-	Polish   string
-	English  string
-	Sentence string
-}
-
-func (r *DatabaseService) CheckWhichDoesntExits(eo ErrorOptions, tx *gorm.DB) error {
-	var word dbmodels.Word
-	var translation dbmodels.Translation
-	var sentence dbmodels.Sentence
-	err := tx.Model(&dbmodels.Word{}).Where("polish = ?", eo.Polish).First(&word).Error
-	if err != nil {
-		if errors.Is(err, gorm.ErrRecordNotFound) {
-			return customerrors.WordNotExistsError{Word: eo.Polish}
-		}
-		return err
-	}
-	err = tx.Model(&dbmodels.Translation{}).Where("english = ? AND word_id = ?", eo.English, word.ID).First(&translation).Error
-	if err != nil {
-		if errors.Is(err, gorm.ErrRecordNotFound) {
-			return customerrors.TranslationNotExistsError{Word: eo.Polish, Translation: eo.English}
-		}
-		return err
-	}
-	if eo.Sentence != "" {
-		err = tx.Model(&dbmodels.Sentence{}).Where("sentence = ? AND translation_id = ?", eo.Sentence, translation.ID).First(&sentence).Error
-		if err != nil {
-			if errors.Is(err, gorm.ErrRecordNotFound) {
-				return customerrors.SentenceNotExistsError{Word: eo.Polish, Translation: eo.English, Sentence: eo.Sentence}
-			}
 			return err
 		}
-	}
-	return fmt.Errorf("nieznany błąd")
+		return nil
+	})
+	return dbmodels.DBWordToGQLWord(&word), nil
 }
--- server/database/dictionaryRepository.go
+++ server/database/dictionaryRepository.go
@@ -3,6 +3,7 @@ package database
 import (
 	"context"
 	"errors"
+	"fmt"
 
 	dbmodels "github.com/staszkiet/DictionaryGolang/server/database/models"
 	customerrors "github.com/staszkiet/DictionaryGolang/server/errors"
@@ -23,6 +24,7 @@ type IDatabase interface {
 }
 
 type dictionaryRepository struct {
+	DB *gorm.DB
 }
 
 func (d *dictionaryRepository) CreateWord(tx *gorm.DB, word *dbmodels.Word) error {
@@ -62,4 +64,158 @@ func (d *dictionaryRepository) AddTranslation(tx *gorm.DB, word *dbmodels.Word, 
 		return err
 	}
 	return nil
+}
+
+func (d *dictionaryRepository) CheckWhichDoesntExits(eo ErrorOptions, tx *gorm.DB) error {
+	var word dbmodels.Word
+	var translation dbmodels.Translation
+	var sentence dbmodels.Sentence
+	err := tx.Model(&dbmodels.Word{}).Where("polish = ?", eo.Polish).First(&word).Error
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return customerrors.WordNotExistsError{Word: eo.Polish}
+		}
+		return err
+	}
+	err = tx.Model(&dbmodels.Translation{}).Where("english = ? AND word_id = ?", eo.English, word.ID).First(&translation).Error
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return customerrors.TranslationNotExistsError{Word: eo.Polish, Translation: eo.English}
+		}
+		return err
+	}
+	if eo.Sentence != "" {
+		err = tx.Model(&dbmodels.Sentence{}).Where("sentence = ? AND translation_id = ?", eo.Sentence, translation.ID).First(&sentence).Error
+		if err != nil {
+			if errors.Is(err, gorm.ErrRecordNotFound) {
+				return customerrors.SentenceNotExistsError{Word: eo.Polish, Translation: eo.English, Sentence: eo.Sentence}
+			}
+			return err
+		}
+	}
+	return fmt.Errorf("nieznany błąd")
+}
+
+func (d *dictionaryRepository) GetSentence(tx *gorm.DB, polish string, english string, sentence string, s *dbmodels.Sentence) error {
+
+	err := tx.Joins("JOIN translations ON sentences.translation_id = translations.id").
+		Joins("JOIN words ON words.id = translations.word_id").
+		Where("words.polish = ? AND translations.english = ? AND sentences.sentence = ?", polish, english, sentence).
+		First(s).Error
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return d.CheckWhichDoesntExits(ErrorOptions{Polish: polish, English: english, Sentence: sentence}, tx)
+		}
+		return err
+	}
+	return nil
+}
+
+func (d *dictionaryRepository) DeleteSentence(tx *gorm.DB, s dbmodels.Sentence) error {
+	if err := tx.Delete(s).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+func (d *dictionaryRepository) GetTranslation(tx *gorm.DB, polish string, english string, translation *dbmodels.Translation) error {
+
+	err := tx.Joins("RIGHT JOIN words ON words.id = translations.word_id").
+		Where("words.polish = ? AND translations.english = ?", polish, english).
+		First(translation).Error
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return d.CheckWhichDoesntExits(ErrorOptions{Polish: polish, English: english}, tx)
+		}
+		return err
+	}
+	return nil
+}
+
+func (d *dictionaryRepository) DeleteTranslation(tx *gorm.DB, translation *dbmodels.Translation) error {
+
+	var count int64
+
+	if err := tx.Model(translation).Delete(&translation).Error; err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	if err := tx.Model(&dbmodels.Translation{}).Where("word_id = ?", translation.WordID).Count(&count).Error; err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	if count == 0 {
+		if err := tx.Where("ID = ?", translation.WordID).Delete(&dbmodels.Word{}).Error; err != nil {
+			tx.Rollback()
+			return err
+		}
+	}
+	return nil
+}
+
+func (d *dictionaryRepository) DeleteWord(tx *gorm.DB, polish string) error {
+
+	if err := tx.Where("polish = ?", polish).Delete(&dbmodels.Word{}).Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return customerrors.WordNotExistsError{Word: polish}
+		}
+		return err
+	}
+	return nil
+}
+
+func (d *dictionaryRepository) UpdateWord(tx *gorm.DB, word *dbmodels.Word, newPolish string) error {
+
+	if err := tx.Model(word).Update("polish", newPolish).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+func (d *dictionaryRepository) UpdateTranslation(tx *gorm.DB, translation *dbmodels.Translation, newEnglish string) error {
+
+	err := tx.Model(translation).Update("english", newEnglish).Error
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (d *dictionaryRepository) UpdateSentence(tx *gorm.DB, sentence *dbmodels.Sentence, newSentence string) error {
+
+	err := tx.Model(sentence).Update("sentence", newSentence).Error
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (d *dictionaryRepository) WithTransaction(fn func(tx *gorm.DB) error) (bool, error) {
+
+	tx := d.DB.Begin()
+	defer func() {
+		if r := recover(); r != nil {
+			tx.Rollback()
+			panic(r)
+		}
+	}()
+
+	if err := tx.Error; err != nil {
+		return false, err
+	}
+
+	if err := fn(tx); err != nil {
+		tx.Rollback()
+		return false, err
+	}
+
+	return true, tx.Commit().Error
+}
+
+type ErrorOptions struct {
+	Polish   string
+	English  string
+	Sentence string
 }
